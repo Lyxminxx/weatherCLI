18ddc8d7c44237315f045b2970df1d3e07ee4aa2
Lyx <madelen@duck.com>
2025-03-15 11:13:56 +0100

    Added molde to the city list

--- possistions.py
+++ possistions.py
@@ -2,5 +2,6 @@ cities = {
     "Oslo" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.9333&lon=10.7166",
     "Trondheim" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=63.4308&lon=10.4034",
     "Skien" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=59.2089&lon=9.606",
-    "Tromsø" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=69.6827&lon=18.9427"
+    "Tromsø" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=69.6827&lon=18.9427",
+    "Molde" : "https://api.met.no/weatherapi/nowcast/2.0/complete?lat=62.73724&lon=7.16073"
 }
